c3895cc5434d6d38786937f4bafbc0233f953a48
Jean-Nicolas Lamarre <jeannicolas.lamarre@gmail.com>
2024-10-18 14:43:29 +0200

    add user input to display exercise specs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring
 
 import duckdb
+import ast
 import pandas as pd
 import streamlit as st
 
@@ -10,29 +11,29 @@ con = duckdb.connect(database="data/exercises_tables.duckdb", read_only=False)
 # SELECT * FROM beverages
 # CROSS JOIN food_prices
 # """
-
-# solution_df = duckdb.sql(ANSWER_STR).df()
+#
+# solution_df = con.execute(ANSWER_STR).df()
 
 
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to revise?",
-        ("cross_joins", "GroupBy", "Window Functions"),
+        ("cross_joins", "GroupBy", "window_functions"),
         index=None,
         placeholder="Selected theme...",
     )
 
     st.write("You selected:", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
 st.header("Your Solution:")
 query = st.text_area(label="Your Query was:", key="user_input")
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
 #
 #     try:
 #         result = result[solution_df.columns]
@@ -47,15 +48,17 @@ query = st.text_area(label="Your Query was:", key="user_input")
 #         )
 #
 #
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_prices")
-#     st.dataframe(food_prices)
-#     st.write("expected")
-#     st.dataframe(solution_df)
-#
-# with tab3:
-#     st.write(ANSWER_STR)
+tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    exercise_tables = ast.literal_eval(exercise.loc[0,"tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
+
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    st.write(answer)
