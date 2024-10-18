3c0e50e7215515b5bb1f42959780b0b0ffa742f6
Jean-Nicolas Lamarre <jeannicolas.lamarre@gmail.com>
2024-10-18 14:52:46 +0200

    comparison with answer

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ import streamlit as st
 
 con = duckdb.connect(database="data/exercises_tables.duckdb", read_only=False)
 
-# ANSWER_STR = """
-# SELECT * FROM beverages
-# CROSS JOIN food_prices
-# """
-#
-# solution_df = con.execute(ANSWER_STR).df()
-
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to revise?",
@@ -28,26 +20,32 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("Your Solution:")
 query = st.text_area(label="Your Query was:", key="user_input")
 
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-#
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"Result has a {n_lines_difference} line differences with the solution"
-#         )
-#
-#
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"Result has a {n_lines_difference} line differences with the solution"
+        )
+
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -58,7 +56,4 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
